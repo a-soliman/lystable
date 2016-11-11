97859742e7316a7cd958b10be6063c13f376b691
Ahmed Soliman <ahmedanwar@mail.com>
2016-11-10 20:47:53 -0800

    coded the animation of the icon at the intro section

--- js/main.js
+++ js/main.js
@@ -17,4 +17,15 @@ $(document).ready(function() {
 		}
 	})
 
+	//animating the icon on Into Section
+	animateed()
+
+	function animateed() {
+		$('.animIcon').animate({ marginTop: '20px'}, 600, function() {
+			$('.animIcon').animate({marginTop: '0'}, 600, function() {
+				setTimeout(animateed(), 600);
+			});
+		});
+	}
+
 });
