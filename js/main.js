20a31c989e9ac546cb54eecea4fc975577b3ef59
Ahmed Soliman <ahmedanwar@mail.com>
2016-11-10 20:47:00 -0800

    coded the fixing of the nav bar

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
 
+	//Nav Bar
+	var introHeight = $('.intro').height()
+	
+	$(window).scroll(function() {
+		
+		if(($(this).scrollTop() > introHeight) && ($(this).width() > 930 )) {
+			$('.navbar').addClass('fixed-nav')
+			$('.navbar img').attr('src', 'img/logo2.png')
+
+		} 
+		else {
+			$('.navbar').removeClass('fixed-nav')
+			$('.navbar img').attr('src', 'img/logo.png')
+
+		}
+	})
 
 });
